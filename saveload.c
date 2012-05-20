5eaa64e8de8c601d6ffb58e60f647eca9de26809
Jaliborc <contact@jaliborc.com>
2012-05-20 21:02:18 +0100

    Starting read & write

--- saveload.c
+++ saveload.c
@@ -1,2 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include "Team.h"
+#include "GameList.h"
 #include "saveload.h"
-static bool readTeams(void); // Internal code function to read teams
+
+
+// Read
+void ReadFiles() {
+	NUM_TEAMS = 50; // I think this should be a #define contant, instead of the size_t wackamole
+
+	ReadTeams();
+	ReadGames();
+};
+
+
+void ReadTeams() {
+	FILE *file = fopen ('WHERE_IS_THE_FILE_SAVED?', 'r');
+	char line[NAME_SIZE * 2];
+	int i = 0;
+	
+	while (fgets(line,1, sizeof(line), file) && i < NUM_TEAMS) {
+		strcpy(Teams[i].name, line);
+		strcpy(Team[i].location, line + NAME_SIZE);
+		i++;
+	};
+};
+
+void ReadGames() {
+	
+};
+
+
+// Write
+void UpdateGames(int start) {
+	
+};
--- saveload.h
+++ saveload.h
@@ -1,20 +1,9 @@
 #ifndef SAVELOAD_H
 #define SAVELOAD_H 1
-#include "datatypes.h"     // Game*
-#include "fixsizedtypes.h" // bool, uint8_t
 
-bool readFiles(void);
+void ReadFiles();
+void ReadTeams();
+void ReadGames();
+void UpdateGames(int); // pass the "index" of the game added/removed in the "main" list
 
-#define ADD_GAME 1
-#define DEL_GAME 2
-/*
-  On updateGames(), if type is:
-	ADD_GAME - adds the game to the games file,
- 	DEL_GAME - deletes the game from the games file
-
-  The game in question is passed as an argument.
-  NOTE: Call this function before removing the game, so that it can still
-  access the whole list to update the info.
- */
-bool updateGames(uint8_t type, Game* game);
 #endif /* SAVELOAD_H */

9e029b00973131db37031cbac1b494ae64371e47
Jaliborc <contact@jaliborc.com>
2012-05-30 23:18:55 +0100

    Dashing forward on game list.

--- globals.c
+++ globals.c
@@ -5,10 +5,11 @@
 int     NUM_TEAMS;
 Team*      Teams;        // All teams, ordered alphabetically
 GameList*  Games;        // Has all games ordered by date
-GameList*  LastGameList; // Points to the last listed game list.
+GameList*  LastGameList; // Points to the last listed game list
 
+GameList NULL_GAME_LIST = {};
 Game NULL_GAME = {
-	0, // outcome
+    0, // outcome
     {0,0,0}, // date
     NULL, // home team
     NULL // home team
--- saveload.c
+++ saveload.c
@@ -14,24 +14,47 @@ void ReadFiles() {
 
 void ReadTeams() {
 	FILE *file = fopen ("WHERE_IS_THE_FILE_SAVED?", "r");
-  char line[NAME_SIZE * 2 + 2];
+  char line[NAME_SIZE * 2];
   
   Teams = malloc(sizeof(Team));
   NUM_TEAMS = 0;
   
-	while (fgets(line, sizeof(line), file)) {
+	while (fgets(line, sizeof(line), file) != NULL) {
+    NUM_TEAMS++;
     realloc(Teams, sizeof(Team) * NUM_TEAMS);
-		NUM_TEAMS++;
     
 		strcpy(Teams[NUM_TEAMS].name, line);
 		strcpy(Teams[NUM_TEAMS].location, line + NAME_SIZE + 1);
 	};
-  
-  
 };
 
 void ReadGames() {
-	
+	FILE *file = fopen ("WHERE_IS_THE_FILE_SAVED?", "r");
+  Games = malloc(sizeof(GameList));
+  
+  GameList* prev = &NULL_GAME_LIST;
+  int i = 0, v;
+  
+  while (fscanf(file, "%d", &v) != EOF) {
+    i++;
+    realloc(Games, sizeof(GameList) * i);
+    
+    GameList* node = &Games[i];
+    Game* game = &node->game;
+    
+    prev->next = node;
+    node->prev = prev;
+    game->homeTeam = &Teams[v];
+    
+    fscanf(file, "%d", &v);
+    fscanf(file, "%d", &game->date.year);
+    fscanf(file, "%d", &game->date.month);
+    fscanf(file, "%d", &game->date.year);
+    fscanf(file, "%d", &game->outcome);
+    
+    game->awayTeam = &Teams[v];
+    prev = node;
+  }
 };
 
 
